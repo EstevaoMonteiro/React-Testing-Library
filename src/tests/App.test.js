5f0613454f48db81d3b0b0bbdae4f0921098510d
EstevaoMonteiro <estevaomonteiroa@gmail.com>
2023-01-11 18:56:18 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,30 @@
-test('', () => {});
-// Pull Request:
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste App.js', () => {
+  test('Se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    expect(linkFavoritePokemon).toBeInTheDocument();
+
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/page');
+    });
+
+    const notFound = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found', level: 2 },
+    );
+    expect(notFound).toBeInTheDocument();
+  });
+});
