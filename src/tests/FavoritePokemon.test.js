556fdd381e8bd4324054b73d4b594d8aeeb7a13a
EstevaoMonteiro <estevaomonteiroa@gmail.com>
2023-01-12 16:47:02 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  test('É exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const pokemonFound = screen.getByText(/No favorite pokémon found/i);
+    expect(pokemonFound).toBeInTheDocument();
+  });
+});
